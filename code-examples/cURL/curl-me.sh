2c583dc8f0c5b0f284b6a0271a3ea73db42a99fe
Victoria <victoriawagman@gmail.com>
2017-03-20 11:25:23 +0100

    Comment details about why 5000000

--- code-examples/cURL/curl-me.sh
+++ code-examples/cURL/curl-me.sh
@@ -1,7 +1,7 @@
 # Get user information
 curl https://<user>:<password>@api-local.46elks.com/a1/Me
 
-# Set level for when credit alert e-mail to be sent
+# Set level for when credit alert e-mail to be sent, when balance goes below 500 (SEK/EUR)
 curl -X POST \
  -u '<user>:<password>' \
  -H 'Content-Type: application/x-www-form-urlencoded' \
