809a18cd393553ad829769ca7d167865b31e7ff5
Victoria <victoria@46elks.com>
2019-03-09 22:43:06 +0100

    Rename sms to voice_setup

--- code-examples/PHP/php-calls.php
+++ code-examples/PHP/php-calls.php
@@ -3,7 +3,7 @@
 $url = 'https://api.46elks.com/a1/Calls';
 $username = '<API Username>';
 $password = '<API Password>';
-$sms = array('from' => '+46709751949',
+$voice_start = array('from' => '+46709751949',
              'to' => '+46709751949',
              'voice_start' => '{"connect":"+461890510"}');
             
@@ -13,7 +13,7 @@ $context = stream_context_create(array(
         'header'  => "Authorization: Basic ".
             base64_encode($username.':'.$password). "\r\n".
             "Content-type: application/x-www-form-urlencoded\r\n",
-        'content' => http_build_query($sms),
+        'content' => http_build_query($voice_start),
         'timeout' => 10
         )
     ));
