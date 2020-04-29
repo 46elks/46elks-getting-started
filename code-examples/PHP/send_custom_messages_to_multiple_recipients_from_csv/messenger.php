8d486ce033b0bbf55221e6bf9b94554c20e013cd
Victoria <victoria@46elks.com>
2020-04-29 15:06:43 +0200

    Update send customized message to multiple recipients from CSV

--- code-examples/PHP/send_custom_messages_to_multiple_recipients_from_csv/messenger.php
+++ code-examples/PHP/send_custom_messages_to_multiple_recipients_from_csv/messenger.php
@@ -1,16 +1,46 @@
 <?php
 
 ## Sending a messages with customized content for each person.
-##
 ## recipients.csv includes a name, phone number, and a code
-## for each person you wish to send this message to,
-## separated with ";"
+## for each person you wish to send this message to separated with ","
+
+# Read from CSV
+$file = fopen("recipients.csv", "r");
+
+// If first line contains headlines, then run this first:
+// fgetcsv($file, 10000, ",");
+
+while ( $row = fgetcsv($file, 13000, ",") ) {
+  list($name, $phone, $code) = $row;
+
+  $message = "Hi". $name ."! \n
+    We're excited to have you with us!\n
+    If you'd like to attend the dinner the evening before,
+    please confirm and let us know of any food preferences by visiting this link:.\n
+    46elks.com/futureisbright-speakers-dinner/".$code."\n\n
+    Regards,\n
+    Marie Curie
+    ";
+  $message = preg_replace('!\s+!', ' ', $message);
+
+  $sms = array(
+    'from' => 'CurieEvent',   // Up to 11 characters (a-z,A-Z,0-9)
+    'to' => $phone,
+    'message' => $message
+  );
+
+  echo sendSMS($sms) . "\n\n";
+
+}
+
+fclose($file);
+
 
 function sendSMS ($sms) {
   // Set your 46elks API username and API password here
   // You can find them at https://dashboard.46elks.com/
-  $username = '<username>';
-  $password = '<password>';
+  $username = 'USERNAME';
+  $password = 'PASSWORD';
   $context = stream_context_create(array(
     'http' => array(
       'method' => 'POST',
@@ -28,32 +58,5 @@ function sendSMS ($sms) {
   return $response;
 }
 
-  # Read from CSV
-  if (($handle = fopen("recipients.csv", "r")) !== FALSE) {
-      while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-          $num = count($data);
-          for ($c=0; $c < $num; $c++) {
-              $this_row = $data[$c];
-              list($name, $phone, $code) = explode(";", $this_row);
 
-              $message = "Hi". $name ."! \n
-We're excited to have you with us!\n
-If you'd like to attend the speakers dinner the evening before,
-please confirm and let us know of any food preferences by visiting this link:.\n
-46elks.com/futureisbright-speakers-dinner/".$code."\n\n
-Regards,\n
-Marie Curie
-";
-            $sms = array(
-              'from' => 'CurieFIB',   /* Can be up to 11 alphanumeric characters (a-z,A-Z,0-9) */
-              'to' => $phone,  /* The mobile number you want to send to */
-              'message' => $message,
-              /* 'flashsms' => 'yes', /* Un comment this if you want to send a flash sms. */
-          );
-          echo sendSMS ($sms) . "\n\n";
-
-          }
-      }
-      fclose($handle);
-  }
 ?>
