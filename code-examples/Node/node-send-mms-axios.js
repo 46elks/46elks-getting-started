5fc7891a1b19ee02cf77b05a4ee388b287c9d6a7
Anton Myrberg <anton@46elks.com>
2022-03-03 16:24:30 +0100

    Added new node examples for MMS

---
+++ code-examples/Node/node-send-mms-axios.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+
+const sendMMS = async(credentials) => {
+    try {
+        // Set the MMS endpoint
+        const url = "https://api.46elks.com/a1/mms";
+
+        // Request data object
+        const data = new URLSearchParams({
+            from: "noreply",
+            to: "+46766860001",
+            message: "Bring a sweater, it’s cold outside!",
+            image: "https://46elks.com/press/46elks-blue-png"
+        });
+
+        // Set the headers
+        const config = {
+            headers: {
+                "Authorization": "Basic " + credentials
+            }
+        };
+
+        // Send request
+        const res = await axios.post(url, data, config);
+        console.log(res.data);
+
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// API credentials
+const username = "APIUSERNAME";
+const password = "APIPASSWORD";
+const authKey = Buffer.from(username + ":" + password).toString("base64");
+
+sendMMS(authKey);
---
+++ code-examples/Node/node-send-mms-fetch.js
@@ -0,0 +1,45 @@
+/**
+ * Sending MMS with fetch
+ *  
+ * 2022-02-24: At the time, fetch() is still an experimental feature in node (since v.17.5).
+ * To run this code without the node-fetch module, use the following command:
+ * node --experimental-fetch node-send-mms-fetch.js
+ * 
+ * Since node-fetch(v.3) does no longer support require().
+ * To use 'import', remember to add "type":"module" in your package.json.
+ * 
+ * For node-fetch(v.2), use:
+ * const fetch = require("node-fetch")
+ * 
+ */
+
+import fetch from "node-fetch";
+
+const sendMMS = async(credentials) => {
+    try {
+        const res = await fetch("https://api.46elks.com/a1/mms", {
+            method: "post",
+            body: new URLSearchParams({
+                from: "noreply",
+                to: "+46766860001",
+                message: "Bring a sweater, it’s cold outside!",
+                image: "https://46elks.com/press/46elks-blue-png"
+            }),
+            headers: { "Authorization": "Basic " + credentials }
+        });
+
+        const mmsRes = await res.json();
+        console.log(mmsRes);
+
+    } catch (err) {
+        console.log(err);
+
+    }
+}
+
+// API credentials
+const username = "APIUSERNAME";
+const password = "APIPASSWORD";
+const authKey = Buffer.from(username + ":" + password).toString("base64");
+
+sendMMS(authKey);
